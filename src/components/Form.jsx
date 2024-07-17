58108f952867d721f6d2bb0cd1fe3c1790712fe9
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-07-17 11:15:40 +0100

    Form part 2: Add the submit button

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,7 +5,7 @@ const Form = () => {
   return (
     <div className="form bg-neutral-white p-8">
       <div className="heading flex justify-between items-center mb-7">
-        <h1 className="text-xl font-bold text-neutral-slate-900">Mortgage Calculator</h1>
+        <h1 className="text-xl font-bold">Mortgage Calculator</h1>
         <button className="text-neutral-slate-700 underline">Clear All</button>
       </div>
       <form action="">
@@ -61,9 +61,26 @@ const Form = () => {
               name="mortgage-type"
               className="radio-input"
             />
-            <label htmlFor={`${id}-mortgage-type-interest`}>Interest Only</label>
+            <label htmlFor={`${id}-mortgage-type-interest`}>
+              Interest Only
+            </label>
           </div>
         </div>
+        <button className="flex items-center gap-3 font-bold bg-primary-lime rounded-full py-2 px-5">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="#133041"
+              d="M18.75 2.25H5.25a1.5 1.5 0 0 0-1.5 1.5v16.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5V3.75a1.5 1.5 0 0 0-1.5-1.5Zm-10.5 16.5a1.125 1.125 0 1 1 0-2.25 1.125 1.125 0 0 1 0 2.25Zm0-3.75a1.125 1.125 0 1 1 0-2.25 1.125 1.125 0 0 1 0 2.25ZM12 18.75a1.125 1.125 0 1 1 0-2.25 1.125 1.125 0 0 1 0 2.25ZM12 15a1.125 1.125 0 1 1 0-2.25A1.125 1.125 0 0 1 12 15Zm3.75 3.75a1.125 1.125 0 1 1 0-2.25 1.125 1.125 0 0 1 0 2.25Zm0-3.75a1.125 1.125 0 1 1 0-2.25 1.125 1.125 0 0 1 0 2.25Zm1.5-5.25a.75.75 0 0 1-.75.75h-9a.75.75 0 0 1-.75-.75V6a.75.75 0 0 1 .75-.75h9a.75.75 0 0 1 .75.75v3.75Z"
+            />
+          </svg>
+          Calculate Repayments
+        </button>
       </form>
     </div>
   );
